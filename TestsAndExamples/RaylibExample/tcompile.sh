4b7439365282d8497dc83730df9230de9ef3b081
!MikaLorielle(미카수키)! <75727770+MikaSukie@users.noreply.github.com>
2026-03-14 02:01:03 +0100

    Update tcompile.sh

--- TestsAndExamples/RaylibExample/tcompile.sh
+++ TestsAndExamples/RaylibExample/tcompile.sh
@@ -6,8 +6,7 @@ python BHUMI.py main.bhumi -o main.ll &&
 opt -O2 main.ll -o main.opt.ll &&
 
 clang -O2 -Wno-override-module main.opt.ll stdlib.c \
-  -I./raylib-5.5_linux_amd64/include \
-  ./raylib-5.5_linux_amd64/lib/libraylib.a \
+  ./lib/libraylib.a \
   -lGL -lm -lpthread -ldl -lrt -lX11 \
   -o main &&
 
