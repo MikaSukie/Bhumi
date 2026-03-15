d734be2cfba53634e5e4359d68d09b27a18d85f7
!MikaLorielle(미카수키)! <75727770+MikaSukie@users.noreply.github.com>
2026-03-15 21:18:18 +0100

    Update tcompile.sh

--- TestsAndExamples/RaylibExample/tcompile.sh
+++ TestsAndExamples/RaylibExample/tcompile.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
 clear
 
-python BHUMI.py main.bhumi -o main.ll &&
-
-opt -O2 main.ll -o main.opt.ll &&
-
-clang -O2 -Wno-override-module main.opt.ll stdlib.c \
-  ./lib/libraylib.a \
-  -lGL -lm -lpthread -ldl -lrt -lX11 \
-  -o main &&
-
-time bash -c './main; echo "Exit code: $?"'
+python BDOP.py &&
+time ./main
